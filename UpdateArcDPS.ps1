61ee17073f4db7f2aadb6ffe205070f083a1c595
gitjoob <social@obergnet.se>
2023-11-08 16:24:04 +0100

    fix

--- UpdateArcDPS.ps1
+++ UpdateArcDPS.ps1
@@ -1,6 +1,6 @@
-wget https://www.deltaconnected.com/arcdps/x64/d3d11.dll -O 'F:\Guild Wars 2\ArcDPS Temp\d3d11.dll'
+wget https://www.deltaconnected.com/arcdps/x64/d3d11.dll -O 'F:\Guild Wars 2\ArcDPSUpdate\d3d11.dll'
 
-$newfile = Get-Item 'F:\Guild Wars 2\ArcDPS Temp\d3d11.dll'
+$newfile = Get-Item 'F:\Guild Wars 2\ArcDPSUpdate\d3d11.dll'
 $currentfile = Get-Item 'F:\Guild Wars 2\d3d11.dll'
 $date = Get-Date -Format yyyy-MM-dd
 
@@ -8,10 +8,10 @@ $version = $currentfile.VersionInfo.FileVersion
 
 if ($newfile.VersionInfo.FileVersion -ne $version)
 {
-    Move-Item 'F:\Guild Wars 2\ArcDPS Temp\d3d11.dll' 'F:\Guild Wars 2\d3d11.dll' -Force
+    Move-Item 'F:\Guild Wars 2\ArcDPSUpdate\d3d11.dll' 'F:\Guild Wars 2\d3d11.dll' -Force
     Write-EventLog -LogName "Application" -Source EventSystem -EventID 6969 -EntryType Information -Message "ArcDPS Updated on $date" -RawData 10,20
 }
 else
 {
-    Remove-Item 'F:\Guild Wars 2\ArcDPS Temp\d3d11.dll'
+    Remove-Item 'F:\Guild Wars 2\ArcDPSUpdate\d3d11.dll'
 }
